434976e5a1fcf36ff69cafcf8cf0a47922f861a2
Elena Gorshenina <elenkagorshenina19.07.14@gmail.com>
2021-06-24 21:25:47 +0300

    First commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int ticketPrice = 115;
+        int bonusMile = 20;
+        int mile = ticketPrice / bonusMile;
+        System.out.println("Mile = " + mile);
+    }
+}
